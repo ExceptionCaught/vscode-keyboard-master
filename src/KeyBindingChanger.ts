03c371dce69860258e6257ff23a64f4de3997433
jonathon.wei <jonathon.wei@gmail.com>
2017-03-24 23:04:59 +1100

    show input box to change key bindings

--- src/KeyBindingChanger.ts
+++ src/KeyBindingChanger.ts
@@ -6,5 +6,10 @@ import {KeyBindQuickPickItem} from "./KeyBindQuickPickItem";
 export function keyBindingChanger() {
     var quickItemList = defaultBindings().map(element => new KeyBindQuickPickItem(element.key, element.command, 'when' in element ? element['when'] : ' '));
     window.showQuickPick(quickItemList, {matchOnDescription:true})
-    .then(selectedKeyBinding => console.log(selectedKeyBinding));
+    .then(function(selectedKeyBinding){
+        window.showInputBox({placeHolder: 'change key binding for ' + selectedKeyBinding.description + ', currently: ' + selectedKeyBinding.label})
+        .then(function(inputValue)){
+
+        });
+    });
 }
